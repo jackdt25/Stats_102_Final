a7d34a3d034512e1f4e72ede43e36b799d9d4798
user_email - readline(prompt = "jackdt25@gmail.com ") <user_name - readline(prompt = "Jack Tyrrell ")>
2025-04-24 18:18:26 -0500

    4/24 - 6:18

--- Stats FInal.R
+++ Stats FInal.R
@@ -26,7 +26,7 @@ prop(~Yes >= obs_diff, data=null_dist) + prop(~Yes <= -obs_diffm data=null_dist)
 library(ggplot2)
 library(tidyverse)
 library(mosaic)
-songs<-read.csv("fight-songs.csv")
+songs <- read.csv("fight-songs.csv")
 head(songs)
 dim(songs)
 count(songs)
@@ -36,10 +36,25 @@ yes_sd <- sd(songs$bpm[songs$victory_win_won=="Yes"])
 yes_sd
 no_mean <-mean(songs$bpm[songs$victory_win_won=="No"]) 
 no_mean
-no_sd<- sd(songs$bpm[songs$victory_win_won=="No"])
+no_sd <- sd(songs$bpm[songs$victory_win_won=="No"])
 no_sd
 ggplot(songs,aes(x=bpm)) +geom_histogram()
 t_stat <- t.test(bpm~victory_win_won, data=songs)
+t_stat
+
+#3-b
+library(dyplr)
+songs2<-filter(songs,school != "Notre Dame") 
+ggplot(songs2, aes(x=conference, y=sec_duration))+geom_point()
+anova(lm(sec_duration~conference, data=songs2))
+songs2_null <-do(10000)*lm(sec_duration~shuffle(conference),data=songs2)
+head(songs2_null)
+p_val <- prop(~(F >= 1.2635),data=songs2_null)
+p_val
+
+
+#3-c
+ggplot(songs, aes(x=year, y=sec_duration)) + geom_point()
 
 
 
